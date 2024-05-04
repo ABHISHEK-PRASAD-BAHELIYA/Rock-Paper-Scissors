ed1f2ef6443d7a30ab94ab63eb95f0e2c1bd3be7
ABHISHEK PRASAD <singhabhishek74401@gmail.com>
2024-05-04 13:32:50 +0530

    creating two functions getComputerChoice & getHumanChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world!");
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const getRandomChoice = Math.floor(Math.random() * choices.length);
+    console.log(choices[getRandomChoice]);
+}
+
+function getHumanChoice() {
+    const enterOwnChoice = prompt("rock, paper, scissors");
+    console.log(enterOwnChoice);
+}
+
+
