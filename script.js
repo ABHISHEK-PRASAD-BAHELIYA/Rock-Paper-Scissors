55b291d9e9aaa0509feced64e53aa8595624bd98
ABHISHEK PRASAD <singhabhishek74401@gmail.com>
2024-05-04 13:37:04 +0530

    Declear the Playerscore variables and write the logic Playground a single round

--- script.js
+++ script.js
@@ -10,4 +10,23 @@ function getHumanChoice() {
     console.log(enterOwnChoice);
 }
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
+    function playGround(humanChoice, computerChoice) {
+        if(humanChoice === computerChoice) {
+            alert(`you both choose ${humanChoice} so, it's a tie.`);
+        } else if(
+            (humanChoice === "rock" && computerChoice === "scissors")||
+            (humanChoice === "paper" && computerChoice === "rock")||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ){
+            alert(`you choose ${humanChoice}. computer choose ${computerChoice}. You win.`);
+            humanScore++;
+        } else {
+            alert(`you choose ${humanChoice}. computer choose ${computerChoice}. You Loose`);
+            computerChoice++;
+        }
+    }
+}
