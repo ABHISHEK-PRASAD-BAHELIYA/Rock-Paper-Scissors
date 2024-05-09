e466f2f778690be900cc600f7461225a5ed7fb71
ABHISHEK PRASAD <singhabhishek74401@gmail.com>
2024-05-09 20:01:50 +0530

    Adding style or Ui with the help of css for project

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
+let showRound = document.querySelector('.round');
+ showRound
 
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const getRandomChoice = Math.floor(Math.random() * choices.length);
-    return choices[getRandomChoice];
-}
 
-function getHumanChoice() {
-    const inputChoice = prompt("Enter your choice : rock, paper, scissors");
-    console.log(inputChoice);
-    return inputChoice;
-}
 
-function playGame() {
-    // Declare the player score variables
-    let humanScore = 0;
-    let computerScore = 0;
+// function getComputerChoice() {
+//     const choices = ["rock", "paper", "scissors"];
+//     const getRandomChoice = Math.floor(Math.random() * choices.length);
+//     return choices[getRandomChoice];
+// }
 
-    function playRound(humanChoice, computerChoice) {
-        if(humanChoice === computerChoice) {
-            alert(`you both choose ${humanChoice} so, it's a tie.`);
-        } else if(
-            (humanChoice === "rock" && computerChoice === "scissors")||
-            (humanChoice === "paper" && computerChoice === "rock")||
-            (humanChoice === "scissors" && computerChoice === "paper")
-        ){
-            alert(`you choose ${humanChoice}. computer choose ${computerChoice}. You win.`);
-            humanScore++;
-        } else {
-            alert(`you choose ${humanChoice}. computer choose ${computerChoice}. You Loose`);
-            computerScore++;
-        }
-    }
+// function getHumanChoice() {
+//     const inputChoice = prompt("Enter your choice : rock, paper, scissors");
+//     console.log(inputChoice);
+//     return inputChoice;
+// }
+
+// function playGame() {
+//     // Declare the player score variables
+//     let humanScore = 0;
+//     let computerScore = 0;
+
+//     function playRound(humanChoice, computerChoice) {
+//         if(humanChoice === computerChoice) {
+//             alert(`you both choose ${humanChoice} so, it's a tie.`);
+//         } else if(
+//             (humanChoice === "rock" && computerChoice === "scissors")||
+//             (humanChoice === "paper" && computerChoice === "rock")||
+//             (humanChoice === "scissors" && computerChoice === "paper")
+//         ){
+//             alert(`you choose ${humanChoice}. computer choose ${computerChoice}. You win.`);
+//             humanScore++;
+//         } else {
+//             alert(`you choose ${humanChoice}. computer choose ${computerChoice}. You Loose`);
+//             computerScore++;
+//         }
+//     }
     //Play the Game 5 times
     for(let i=1; i<=5; i++) {
         alert(`Round ${i}. Get Ready!`);
@@ -40,17 +44,17 @@ function playGame() {
         playRound(humanSelection, computerSelection);
         alert(`Your score: ${humanScore}. computer Score: ${computerScore}.`);
     }
-    if(humanScore < computerScore) {
-        alert("You loose the Game.");
-    } else if(humanScore > computerScore){
-        alert("You won the Game.");
-    } else {
-        alert("Too Bad, the game was tie...");
-    }
+//     if(humanScore < computerScore) {
+//         alert("You loose the Game.");
+//     } else if(humanScore > computerScore){
+//         alert("You won the Game.");
+//     } else {
+//         alert("Too Bad, the game was tie...");
+//     }
 
-}
-console.log(playGame())
-playGame();
+// }
+// console.log(playGame())
+// playGame();
 
 
 
