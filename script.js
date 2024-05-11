6b4ce21785905cb9eda5d269f7234d98ec4947e4
ABHISHEK PRASAD <singhabhishek74401@gmail.com>
2024-05-11 18:28:56 +0530

    complete script.js files with code

--- script.js
+++ script.js
@@ -2,26 +2,61 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll('.choice');
+const msgPara = document.querySelector('#msg');
 
+const userScorePara = document.querySelector('#user-score');
+const compScorePara = document.querySelector('#comp-score');
 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
     const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
-}
+};
 
 const drawGame = () => {
     console.log("game was draw.")
-}
+    msgPara.innerText = ("Game was Draw. play again.")
+    msgPara.style.backgroundColor = "#081b31"
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if(userWin) {
+        userScore++;
+        userScorePara.innerText = `Player: ${userScore}`
+        console.log("YOU WON ! 🎉");
+        msgPara.innerText = `You won ! 🎉 Your ${userChoice} beats ${ compChoice}`;
+        msgPara.style.backgroundColor = "green";
+    } else {
+        compScore++;
+        compScorePara.innerText = `Computer: ${compScore}`;
+        console.log("YOU LOOSE !");
+        msgPara.innerText = `You lost. :-( ${compChoice} beats Your ${userChoice}`;
+        msgPara.style.backgroundColor = "red";
+    }
+};
 
 const playGame = (userChoice) => {
     console.log("user choice = ", userChoice);
     //generate computer choice --> modular
     const compChoice = genCompChoice();
-    console.log(compChoice);
+    console.log("comp choice = ", compChoice);
 
     if(userChoice === compChoice) {
+        //Draw Game
         drawGame();
+    } else {
+        let userWin = true;
+        if(userChoice === "rock") {
+            //scissors, paper
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            //rock, scissors
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            //rock, paper
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
     }
 };
 
